cff2baf7c9ddd2e2bf2bf55240d0983ee462834a
Evgeniy <260443@mail.ru>
2023-07-10 20:18:47 +0500

    Breeze and Vite

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite(['resources/sass/main.scss', 'resources/js/main.js'])
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="col-md-8">
+                <p>111</p>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
